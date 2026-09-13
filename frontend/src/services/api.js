b83bde07001813d558c3d7f13b4e4973402ff962
melanylondono888-ops <melanylondono888@gmail.com>
2026-09-13 18:33:23 -0500

    fix: conectar frontend con backend de Render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://anime-studioghibli-catalogo.onrender.com/api'
 });
 
 export const getGhibliMovies = () => API.get('/ghibli');
